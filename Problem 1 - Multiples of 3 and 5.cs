9c7e5648a2c7043c05eff3c9ce149d0d463f5724
Kunzi Mariano <kunzi.mariano@gmail.com>
2013-01-03 00:27:03 -0300

    Problem 1 - Multiples of 3 and 5

---
+++ Problem 1 - Multiples of 3 and 5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecteuler.net
+{
+    public class Problem1
+    {
+        public static void Multiplesof3And5()
+        {
+            int result = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    result += i;
+            }
+
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecteuler.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1.Multiplesof3And5();
+        }
+    }
+}
