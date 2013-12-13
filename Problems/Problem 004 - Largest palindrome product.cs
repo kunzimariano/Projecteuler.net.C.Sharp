8ed52f813289fab9d886a62970ef89d49f974e61
Kunzi Mariano <kunzi.mariano@gmail.com>
2013-12-13 02:55:30 -0300

    Partial refactoring for problem 4.

--- Problems/Problem 004 - Largest palindrome product.cs
+++ Problems/Problem 004 - Largest palindrome product.cs
@@ -8,53 +8,54 @@ namespace ConsoleApp.Problems
     {
         public static void First()
         {
-            int largest = 0;
-
+            int largestPalindrome = 0;
+            // TODO: this needs more refactoring
             for (int i = 999; i > 99; i -= 2)
                 for (int j = i - 1; j > 99; j--)
                 {
-                    int pal = i * j;
-                    if (IsPalindrome(pal) && largest < pal)
+                    int palindromeCandidate = i * j;
+                    if (IsPalindrome(palindromeCandidate) && largestPalindrome < palindromeCandidate)
                     {
-                        largest = pal;
+                        largestPalindrome = palindromeCandidate;
                         break;
                     }
-
                 }
-
-            Console.WriteLine(largest);
-
+            Console.WriteLine(largestPalindrome);
         }
 
-        private static bool IsPalindrome(int pal)
+        private static bool IsPalindrome(int candidate)
         {
-            string s = pal.ToString();
-            //zero-indexed end of the string
+            string s = candidate.ToString();
+            if (IsEven(s))
+                return CheckEven(s);
+            return CheckOdd(s);
+        }
+
+        private static bool IsEven(string s)
+        {
+            return s.Length % 2 == 0;
+        }
+
+        private static bool CheckEven(string s)
+        {
             int end = s.Length - 1;
-            int leftLimit;
+            int leftLimit = end / 2;
 
+            for (int i = 0, j = end; i <= leftLimit; i++, j--)
+                if (s[i] != s[j])
+                    return false;
+            return true;
+        }
 
-            // even
-            if (s.Length % 2 == 0)
-            {
-                leftLimit = end / 2;
+        private static bool CheckOdd(string s)
+        {
+            int end = s.Length - 1;
+            int leftLimit = end / 2 - 1;
 
-                for (int i = 0, j = end; i <= leftLimit; i++, j--)
-                    if (s[i] != s[j])
-                        return false;
-                return true;
-            }
-            // odd
-            else
-            {
-                leftLimit = end / 2 - 1;
-
-                for (int i = 0, j = end; i <= leftLimit; i++, j--)
-                    if (s[i] != s[j])
-                        return false;
-                return true;
-
-            }
+            for (int i = 0, j = end; i <= leftLimit; i++, j--)
+                if (s[i] != s[j])
+                    return false;
+            return true;
         }
     }
 }
